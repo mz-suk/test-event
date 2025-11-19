a5e6b05a36511b90b4fb04f674b4584e13bbe04a
suk <suk@mz.co.kr>
2025-11-19 15:20:42 +0900

    init

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Lenis from 'lenis';
+import { createApp } from 'vue';
+import App from './App.vue';
+import './assets/main.css';
+
+const lenis = new Lenis({
+  lerp: 0.08,
+  duration: 1.1,
+  smoothWheel: true,
+  wheelMultiplier: 1.05,
+  smoothTouch: false,
+});
+
+lenis.on('scroll', event => {
+  window.dispatchEvent(new CustomEvent('smooth-scroll', { detail: event }));
+});
+
+const loop = time => {
+  lenis.raf(time);
+  requestAnimationFrame(loop);
+};
+
+requestAnimationFrame(loop);
+
+createApp(App).mount('#app');
